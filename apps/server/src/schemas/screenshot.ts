dd87ea8a0e89cc232a0af90fce15e2d6107038d5
tigro7 <alby.tiri@gmail.com>
2024-12-03 17:39:04 +0100

    creato schema mongoose per screenshot

---
+++ apps/server/src/schemas/screenshot.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IScreenshot extends Document {
+    url: string;
+    file: Blob;
+    status: 'queued' | 'processing' | 'done';
+}
+
+const ScreenshotSchema: Schema = new Schema({
+    url: { type: String, required: true },
+    file: { type: Blob, required: true },
+    status: { type: String, 
+        enum: ['queued', 'processing', 'done'],
+        required: true,
+        default: 'queued'
+    }
+});
+
+const Screenshot = mongoose.model<IScreenshot>('Screenshot', ScreenshotSchema);
+
+export default Screenshot;
