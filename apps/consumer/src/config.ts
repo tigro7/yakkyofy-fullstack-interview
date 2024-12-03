792baa7348df408e3dbaf21c12ada08fcd882d9d
tigro7 <alby.tiri@gmail.com>
2024-12-03 17:18:59 +0100

    create istanze di mongo e rabbit con docker run --name mongo -p 27017:27017 -v mongo_data:/data/db -d mongo:6.0 docker run --name rabbitmq -p 5672:5672 -p 15672:15672 -v rabbitmq_data:/var/lib/rabbitmq -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest -d rabbitmq:3-management sistemati file config.ts per server e consumer e fatti partire con npm run dev dalle tre root

--- apps/consumer/src/config.ts
+++ apps/consumer/src/config.ts
@@ -4,6 +4,6 @@ dotenv.config()
 export default {
   ENV: process.env.NODE_ENV || 'development',
   BASE_DIR: __dirname,
-  RABBIT_MQ: process.env.RABBIT_MQ || 'amqp://localhost',
+  RABBIT_MQ: process.env.RABBIT_MQ || 'amqp://guest:guest@localhost',
   MONGO_URL: process.env.MONGO_URL || ''
 }
--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -4,8 +4,8 @@ dotenv.config()
 export default {
   ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
-  MONGO_URL: process.env.MONGO_URL || '',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/interview',
   JWT_SECRET: process.env.JWT_SECRET || 'secret',
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'r_secret',
-  RABBIT_MQ: process.env.RABBIT_MQ || ''
+  RABBIT_MQ: process.env.RABBIT_MQ || 'amqp://guest:guest@localhost',
 }
